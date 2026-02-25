90efbc61cef57a64397fcbb4df805e2234404c14
23033ln <23033ln@hvhs.school.nz>
2026-02-26 10:52:33 +1300

    0.3

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
-function setup{
+function setup(){
     cnv = new Canvas(100, 100);
     cube = new Sprite(50, 50, 50, 50);
 
 cube.color = 'green';
 }
-function draw{
+function draw(){
      background = lightgreen;
 }
