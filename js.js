4de2f654f26562f70bd1929da76e0b963adcc1bf
23033ln <23033ln@hvhs.school.nz>
2026-02-27 12:06:45 +1300

    0.6

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 function setup() {
 	console.log("setup: ");
-     cnv = new Canvas(1920, 1080);
+     cnv = new Canvas(1000, 1000);
 	  rect = new Sprite(0, 0, 100, 400);
       rect = color = 'red';
 	  cir = new Sprite(100, 100, 100);
