9c12e975b875f55484625d97bdab7995deb546e5
23033ln <23033ln@hvhs.school.nz>
2026-03-02 13:18:35 +1300

    0.9

--- game.js
+++ game.js
@@ -1,22 +1,32 @@
 function setup() {
 	console.log("setup: ");
-     cnv = new Canvas(1000, 1000);
-	  rect = new Sprite(20, 20, 100, 400);
-      rect = color = 'red';
-        rect = new Sprite(20, 20, 100, 400);
-      rect = color = 'pink';
-      world.gravity.y = 10;
+     cnv = new Canvas(1200, 1100);
+	 rect = new Sprite(20, 20, 400, 100);
+     rect = color = 'red';
+     cube = new Sprite(20, 20, 100, 100, 'd');
+     cube.color = 'blue';
+     world.gravity.y = 10;
 }
 function draw(){
     background('lightgreen'); 
-     if(kb.pressing(Space,up,w)){
-        vel = +3
+     if(kb.pressing(Space)){
+       vel = +3;
      } else {
-        vel = -1
+       vel = -1;
      }
      if (mouse.presses(leftclick)) {
-         vel = +3
-    } else {
-        vel = -1
-    }
+       vel = +3;
+     } else {
+       vel = -1;
+     }
+     if(kb.presses(w)) {
+       vel = +3;
+     } else {
+       vel = -1;
+     }
+     if (kb.presses(up)) {
+       vel = +3;
+     } else {
+       vel = -1;
+     }
 }
