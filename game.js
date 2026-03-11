d4533bc23046f0600c47eff8e76cf91b81a77ec5
23033ln <23033ln@hvhs.school.nz>
2026-03-12 10:22:06 +1300

    2.4

--- game.js
+++ game.js
@@ -9,10 +9,10 @@ function setup() {
   floor = new Sprite(width / 2, 700, 1390, 40, 'k');
   floor.color = 'yellow';
   //BLock that the cube can die on
-  spike = new Sprite(1000, 660, 40, 40, 'k');
+  spike = new Sprite(1000, 660, 10, 40, 'k');
   spike.color = 'red';
   spike.vel.x = -2;
-  spike = new Sprite(1400, 660, 40, 40, 'k');
+  spike = new Sprite(1400, 660, 10, 40, 'k');
   spike.color = 'red';
   spike.vel.x = -2;
   spike.friction = 0;
@@ -20,13 +20,17 @@ function setup() {
   block = new Sprite(1600, 640, 40, 80, 'k');
     block.color = 'pink';
   block.vel.x = -2;
-  block = new Sprite(1640, 640, 40, 40, 'k');
+  block = new Sprite(1640, 620, 40, 40, 'k');
   block.color = 'pink';
   block.vel.x = -2;
-    block = new Sprite(3000, 640, 40, 40, 'k');
+    block = new Sprite(1680, 620, 40, 40, 'k');
   block.color = 'pink';
-  block.vel.x = -100;
-   
+  block.vel.x = -2;
+  //the progress bar and the numbers that you see how far you have gone
+   progress = new Sprite(width/2, 30, 800, 50, 'k');
+   progress.color = 'orange';
+   progress = new Sprite(260, 30, 50, 50, 'k');
+   progress.color = 'purple';
 
   block.friction = 0;
   //gravity
@@ -37,11 +41,11 @@ function draw() {
   background('green');
   if (kb.pressing('up')) {
     if (cube.vel.y = ('0')) {
-     cube.vel.y = -4
+     cube.vel.y = -8
     }
   }else if (kb.released('up')) {
          if (cube.vel.y = ('0')) {
-     cube.vel.y = +0.5
+     cube.vel.y = +1
     }
    }
 }
