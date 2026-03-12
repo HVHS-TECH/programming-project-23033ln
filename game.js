3dab57156c28496fb2e264472f55edb4708a6776
23033ln <23033ln@hvhs.school.nz>
2026-03-13 12:26:39 +1300

    2.6

--- game.js
+++ game.js
@@ -1,34 +1,60 @@
 function setup() {
-  console.log("setup: ");
+  console.log("setup: fffffgfgggfggfgffggfg ");
   console.log("setup: dasdasfefsef");
   cnv = new Canvas(1390, 900);
-
+  // timers for the up and space//
+  timeUP = 3
   // the cube that the player uses and the floor of the game//
   cube = new Sprite(width - 1300, 600, 40, 40, 'd');
   cube.color = 'blue';
   cube.friction = 0;
   floor = new Sprite(width / 2, 700, 1390, 40, 'k');
-  floor.color = 'yellow';
+  floor.color = 'cyan';
 
   //BLock that the cube can die on//
-  spike = new Sprite(1000, 660, 10, 40, 'k');
+  spike = new Sprite(900, 665, 10, 30, 'k');
   spike.color = 'red';
-  spike.vel.x = -2;
-  spike = new Sprite(1400, 660, 10, 40, 'k');
+  spike.vel.x = -3;
+
+  spike = new Sprite(940, 665, 10, 30, 'k');
   spike.color = 'red';
-  spike.vel.x = -2;
+  spike.vel.x = -3;
+
+  spike = new Sprite(1760, 545, 10, 30, 'k');
+  spike.color = 'red';
+  spike.vel.x = -3;
+
   spike.friction = 0;
 
   //blocks that the cube can stand on//
+  block = new Sprite(1520, 660, 40, 40, 'k');
+  block.color = 'pink';
+  block.vel.x = -3;
+
+  block = new Sprite(1560, 660, 40, 40, 'k');
+  block.color = 'pink';
+  block.vel.x = -3;
+
   block = new Sprite(1600, 640, 40, 80, 'k');
   block.color = 'pink';
-  block.vel.x = -2;
+  block.vel.x = -3;
+
   block = new Sprite(1640, 620, 40, 40, 'k');
   block.color = 'pink';
-  block.vel.x = -2;
+  block.vel.x = -3;
+
   block = new Sprite(1680, 620, 40, 40, 'k');
   block.color = 'pink';
-  block.vel.x = -2;
+  block.vel.x = -3;
+
+  block = new Sprite(1940, 540, 40, 40, 'k');
+  block.color = 'pink';
+  block.vel.x = -3;
+
+  block = new Sprite(2450, 580, 1500, 40, 'k');
+  block.color = 'pink';
+  block.vel.x = -3;
+
   block.friction = 0;
 
   //the progress bar and the numbers that you see how far you have gone//
@@ -38,7 +64,8 @@ function setup() {
   number.color = 'grey';
   progress = new Sprite(width / 2, 30, 800, 50, 'k');
   progress.color = 'black';
-
+  // let barWidth = map(currentValue,0 ,100, 0 ,100)//
+  //shorterWidth//
   //gravity//
   world.gravity.y = 10;
 
@@ -49,15 +76,17 @@ function draw() {
     if (cube.vel.y = ('0')) {
       cube.vel.y = -8
     }
+    timeUP = timeUP = - 1
   } else if (kb.released('up')) {
     if (cube.vel.y = ('0')) {
       cube.vel.y = +1
     }
+    timeUP = 3
   }
-  if (cube>= block, spike) {
-   if (cube.vel.x = ('0')) {
-      progress.vel.x = +0.1
+  if (cube >= block, spike) {
+    if (cube.vel.x = ('0')) {
+      progress.vel.x = +0.060
     }
   }
- 
+
 }
