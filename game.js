cc3daeffb19633f1553ba23fea23b2ee1e4b9fc7
23033ln <23033ln@hvhs.school.nz>
2026-03-19 10:54:53 +1300

    2.9(changed the code alittle bit)

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ function setup() {
   cnv = new Canvas(1390, 900);
 
   // timers for the up and space//
-  timeUP = 3
+  var timeUP = 3
   // the cube that the player uses and the floor of the game//
   cube = new Sprite(width - 1300, 600, 40, 40, 'd');
   cube.color = 'blue';
@@ -66,13 +66,10 @@ function setup() {
   progress = new Sprite(width / 2, 30, 800, 50, 'k');
   progress.color = 'black';
 
-  //let barWidth = map(100,0 ,100, 0 ,100)//
-
   //gravity//
   world.gravity.y = 10;
   //score//
   var score = 0;
-  
   
 }
 function draw() {
@@ -92,11 +89,10 @@ function draw() {
     if (cube.vel.x = ('0')) {
       progress.vel.x = +0.100
       progress.width = progress.width - 0.200
-      //progress = barWidth;//
     }
   }
    if (score = progress.vel.x ) {
-    score = score + 0.10;
+    score = score + 0.01;
    } 
    text('' + score ,  230, 20, 50);
    textSize(30);
